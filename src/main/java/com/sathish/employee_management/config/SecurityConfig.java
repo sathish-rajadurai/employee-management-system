b96d143a458e24915dc45d2d358ec746e8381829
Sathish Rajadurai <rajadurai.sathish@gmail.com>
2026-09-03 22:29:59 +0530

    Add Actuator health check

--- src/main/java/com/sathish/employee_management/config/SecurityConfig.java
+++ src/main/java/com/sathish/employee_management/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/health"
                         ).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/employees/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/v1/employees/**").hasRole("ADMIN")
